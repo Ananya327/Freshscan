19d15c44ed1acc401474787a83a7128c5e034074
Ananya327 <naikananya02@gmail.com>
2025-06-22 17:10:09 +0530

    Created index view to load homepage

--- detector/views.py
+++ detector/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+echo "from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')" > detector/views.py
