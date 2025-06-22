87de5850e26b5d2b35d7f59cafc1599c4a7e76f8
Ananya327 <naikananya02@gmail.com>
2025-06-22 18:20:56 +0530

    detector  view file is updated  added

--- detector/views.py
+++ detector/views.py
@@ -1,7 +1,6 @@
+# detector/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
-echo "from django.shortcuts import render
-
 def index(request):
-    return render(request, 'index.html')" > detector/views.py
+    return render(request, 'index.html')
