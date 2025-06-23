d04399dff91fd19e8688154bfe4495e09b12f927
Ananya327 <naikananya02@gmail.com>
2025-06-23 21:24:18 +0530

    Created upload.html page and redirect from index to upload

--- detector/views.py
+++ detector/views.py
@@ -1,6 +1,16 @@
-# detector/views.py
-
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponseRedirect
+import os
 
 def index(request):
     return render(request, 'index.html')
+
+def detect(request):
+    if request.method == 'POST':
+        return render(request, 'upload.html')  # Show file upload page
+
+    if request.method == 'GET':
+        return HttpResponseRedirect('/')  # If accessed directly, go home
+
+    return HttpResponse("Invalid request.")
