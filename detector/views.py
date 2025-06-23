5756bd03f3f90652bc5a913a772ac9f246ffddd0
Ananya327 <naikananya02@gmail.com>
2025-06-23 21:45:12 +0530

    Added dummy prediction model and upload/result views

--- detector/views.py
+++ detector/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect
+from .model import predict_fruit_or_vegetable
 import os
 
 def index(request):
     return render(request, 'index.html')
 
 def detect(request):
-    if request.method == 'POST':
-        return render(request, 'upload.html')  # Show file upload page
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(image.name, image)
+        image_url = fs.url(filename)
 
-    if request.method == 'GET':
-        return HttpResponseRedirect('/')  # If accessed directly, go home
+        # Predict using dummy model
+        prediction, calorie = predict_fruit_or_vegetable(fs.path(filename))
 
-    return HttpResponse("Invalid request.")
+        return render(request, 'result.html', {
+            'image_url': image_url,
+            'prediction': prediction,
+            'calorie': calorie
+        })
+    return render(request, 'upload.html')
